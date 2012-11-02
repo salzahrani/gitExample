12e5b72a2e5c21d3ca7cb1c801584bbfd65de851
Sultan <salzahrani@gmail.com>
2012-11-02 11:53:39 -0700

    Adding a second line
    some updates in line 2

--- gitExample/src/GitExample.java
+++ gitExample/src/GitExample.java
@@ -1,4 +1,3 @@
-
 public class GitExample {
 
 	/**
@@ -7,6 +6,8 @@ public class GitExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is line 1");
+                  System.out.println("This is line 2");
+
 	}
 
 }
