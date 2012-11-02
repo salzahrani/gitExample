c95c43529ed3e43ad53181c6331280fb48788929
Sultan <salzahrani@gmail.com>
2012-11-02 12:06:40 -0700

    version1

--- gitExample/src/GitExample.java
+++ gitExample/src/GitExample.java
@@ -6,7 +6,8 @@ public class GitExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is line 1");
-                  System.out.println("This is line 2");
+        System.out.println("This is line 2");
+        System.out.println("This is line 3");
 
 	}
 
