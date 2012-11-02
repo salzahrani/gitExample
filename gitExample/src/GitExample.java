6c82679d50e75705622d2cca7ca7254d46280fd4
Sultan <salzahrani@gmail.com>
2012-11-02 11:46:18 -0700

    version2

---
+++ gitExample/src/GitExample.java
@@ -0,0 +1,12 @@
+
+public class GitExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is line 1");
+	}
+
+}
